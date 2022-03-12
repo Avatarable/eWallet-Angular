b28ddfa1760671059aa6414ca4ec9877537f4795
Able <avatarable@gmail.com>
2022-03-12 12:56:33 +0100

    removed public, corrected lordicons on features

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { ManagerComponent } from './pages/manager/manager.component';
-import { PublicComponent } from './pages/public/public.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
   { path: 'admin', component: AdminComponent },
   { path: 'manager', component: ManagerComponent },
-  { path: 'public', component: PublicComponent },
 ];
 
 @NgModule({
